9c86adbc88a424ac94dcb55a8cc5025788f554ab
Roman Oliinyk <inthewavves@gmail.com>
2024-10-06 17:56:56 +0200

    feat: failInstance method

--- Pizza.rb
+++ Pizza.rb
@@ -1,5 +1,6 @@
 class Pizza
     @@count = 0
+    @@failer_count = 0
     def initialize d, ingredients
         if d == 25 || d == 50
             @d = d
@@ -10,11 +11,16 @@ class Pizza
             @d = 0
             @number = 0
             @ingredients = []
+            @@failer_count += 1
         end 
     end
 
     def description
         "Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients.join(',')}]."
+    end
+
+    def self.failInstance
+        @@failer_count
     end
 end
 
@@ -22,3 +28,4 @@ pizza1 = Pizza.new(24, ['mushroom', 'cheese'])
 pizza2 = Pizza.new(25, ['mushroom', 'cheese', 'cherry'])
 puts pizza2.description #=>Піца № 1 (діагональ = 25) містить [mushroom,cheese,cherry].
 puts pizza1.description #=>Піца № 0 (діагональ = 0) містить [].
+puts Pizza.failInstance
