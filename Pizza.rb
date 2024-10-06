bb84ef95f5674f1c423a1ad1a88bcc8e95655f55
Roman Oliinyk <inthewavves@gmail.com>
2024-10-06 17:24:39 +0200

    feat: test cheker

--- Pizza.rb
+++ Pizza.rb
@@ -11,3 +11,7 @@ class Pizza
         puts "Піца № #{@number} (діагональ = #{@d}) містить [#{@ingredients.join(',')}]."
     end
 end
+
+pizza1 = Pizza.new(24, ['mushroom', 'cheese'])
+pizza2 = Pizza.new(25, ['mushroom', 'cheese', 'cherry'])
+puts pizza2.description #=>Піца № 1 (діагональ = 25) містить [mushroom,cheese,cherry].
