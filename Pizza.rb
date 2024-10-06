6cdfeb2ca789521965e798c9dbf9eb4ebee17841
Roman Oliinyk <inthewavves@gmail.com>
2024-10-06 17:34:45 +0200

    fix: wrong diameter case

--- Pizza.rb
+++ Pizza.rb
@@ -1,10 +1,16 @@
 class Pizza
     @@count = 0
     def initialize d, ingredients
-        @d = d
-        @ingredients = ingredients
-        @@count += 1 
-        @number = @@count 
+        if d == 25 || d == 50
+            @d = d
+            @ingredients = ingredients
+            @@count += 1 
+            @number = @@count
+        else
+            @d = 0
+            @number = 0
+            @ingredients = []
+        end 
     end
 
     def description
@@ -15,3 +21,4 @@ end
 pizza1 = Pizza.new(24, ['mushroom', 'cheese'])
 pizza2 = Pizza.new(25, ['mushroom', 'cheese', 'cherry'])
 puts pizza2.description #=>Піца № 1 (діагональ = 25) містить [mushroom,cheese,cherry].
+puts pizza1.description #=>Піца № 0 (діагональ = 0) містить [].
